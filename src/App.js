68f23064c43ffb0dabdad157c2df8771ac68a006
Antonio Medina <toomedinaamp2010@gmail.com>
2022-01-09 21:22:42 -0400

    App bar added and search modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import Home from './components/pages/Home'
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/pages/Home";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#8ab8bd",
+    },
+    secondary: {
+      main: "#2b828a",
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Home>
-          Hola
-        </Home>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div>
+          <Home>Hola</Home>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,71 +1,98 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
 import axios from "axios";
 import {
+  alpha,
+  AppBar,
   FormControl,
+  Grid,
   InputBase,
   InputLabel,
   MenuItem,
   Select,
+  Toolbar,
+  Typography,
 } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import SearchIcon from "@material-ui/icons/Search";
 
-const columns = [
-  { id: "song", label: "Song", minWidth: 170 },
-  { id: "artist", label: "Artist", minWidth: 100 },
-  {
-    id: "album",
-    label: "Album",
-    minWidth: 170,
-  },
-  {
-    id: "time",
-    label: "Time",
-    minWidth: 170,
-  },
-  {
-    id: "price",
-    label: "Price",
-    minWidth: 170,
-  },
-];
-
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
+    flexGrow: 1,
   },
   container: {
     maxHeight: 440,
   },
-});
+  paper: {
+    height: "300px !important",
+    textAlign: "center",
+    background: "#d0d7db",
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+}
 
 export default function Home() {
   const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   const [songs, setSongs] = useState([]);
   const [search, setSearch] = useState("");
   const [attribute, setAttribute] = useState("all");
 
   const baseURL = "https://itunes.apple.com/search";
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   const artistGet = async () => {
     await axios
@@ -91,70 +118,83 @@ export default function Home() {
   };
 
   return (
-    <div style={{ padding: "50px" }}>
-      <div>
-        <InputBase
-          value={search}
-          placeholder="Search input"
-          onChange={handleChange}
-        />
-        <FormControl>
-          <InputLabel>Attribute</InputLabel>
-          <Select value={attribute} onChange={handleAttributeChange}>
-            <MenuItem value={"all"}>All</MenuItem>
-            <MenuItem value={"music"}>Music</MenuItem>
-            <MenuItem value={"movie"}>Movie</MenuItem>
-          </Select>
-        </FormControl>
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.button}
-          onClick={() => artistGet()}
-        >
-          Search
-        </Button>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Grid container justifyContent="space-between" alignItems="center">
+            <Grid item xs={12} xl={4} md={4} sm={4}>
+              <Typography variant="h6" color="inherit">
+                Search Content
+              </Typography>
+            </Grid>
+            <Grid item xs={12} xl={3} md={4} sm={4}>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  onChange={handleChange}
+                  value={search}
+                  placeholder="Search…"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </div>
+            </Grid>
+            <Grid item xs={12} xl={3} md={2} sm={2}>
+              <FormControl>
+                <InputLabel>Attribute</InputLabel>
+                <Select value={attribute} onChange={handleAttributeChange}>
+                  <MenuItem value={"all"}>All</MenuItem>
+                  <MenuItem value={"music"}>Music</MenuItem>
+                  <MenuItem value={"movie"}>Movie</MenuItem>
+                  <MenuItem value={"podcast"}>Podcast</MenuItem>
+                  <MenuItem value={"musicVideo"}>Music Video</MenuItem>
+                  <MenuItem value={"audiobook"}>Audiobook</MenuItem>
+                  <MenuItem value={"tvShow"}>Tv Show</MenuItem>
+                  <MenuItem value={"software"}>Software</MenuItem>
+                  <MenuItem value={"shortFilm"}>Short Film</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} xl={2} md={2} sm={2}>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={() => artistGet()}
+              >
+                Search
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <div style={{ padding: "50px" }}>
+        <Grid container justifyContent="center" spacing={2}>
+          {songs &&
+            songs.map((song, index) => (
+              <Grid key={index} item xs={12} sm={6} xl={3} md={4}>
+                <Paper className={classes.paper}>
+                  <Typography>{song.trackName}</Typography>
+                  <Typography>{song.artistName}</Typography>
+                  <Typography>{song.collectionName}</Typography>
+                  <Typography>
+                    {millisToMinutesAndSeconds(song.trackTimeMillis)}
+                  </Typography>
+                  <Typography>{song.trackPrice}</Typography>
+                  <img
+                    className={classes.img}
+                    alt="complex"
+                    src={song.artworkUrl100}
+                  />
+                </Paper>
+              </Grid>
+            ))}
+        </Grid>
       </div>
-      <Paper className={classes.root}>
-        <TableContainer className={classes.container}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell
-                    key={column.id}
-                    align={column.align}
-                    style={{ minWidth: column.minWidth }}
-                  >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {songs &&
-                songs.map((song, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{song.trackName}</TableCell>
-                    <TableCell>{song.artistName}</TableCell>
-                    <TableCell>{song.collectionName}</TableCell>
-                    <TableCell>{song.trackTimeMillis}</TableCell>
-                    <TableCell>{song.trackPrice}</TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={songs.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-    </div>
+    </>
   );
 }
